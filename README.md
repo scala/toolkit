cd103410f3cac210c82ece73891410418cb62e0d
Szymon Rodziewicz <szymonrodant@gmail.com>
2023-01-05 17:59:21 +0100

    Publishing Toolkit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# scala-toolkit
+# Scala Toolkit
+The batteries-included Scala. Read about Scala Toolkit in [Scala Toolkit makes Scala powerful straight out of the box](https://virtuslab.com/blog/scala-toolkit-makes-scala-powerful-straight-out-of-the-box/).
---
+++ publish-conf.scala
@@ -0,0 +1,10 @@
+//> using publish.organization "org.virtuslab"
+//> using publish.name "toolkit-alpha"
+//> using publish.computeVersion "git:tag"
+//> using publish.url "https://github.com/VirtusLab/toolkit"
+//> using publish.versionControl "scm:git:github.com/VirtusLab/toolkit.git"
+//> using publish.license "Apache-2.0"
+//> using publish.repository "central"
+//> using publish.developer "szymon-rd|Simon R|https://github.com/szymon-rd"
+//> using publish.secretKey "env:PUBLISH_SECRET_KEY"
+
