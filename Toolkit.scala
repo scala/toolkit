73cec0c797c93c1c74befeb22056babaf3d94579
Szymon Rodziewicz <szymonrodant@gmail.com>
2023-01-05 13:30:50 +0100

    Initialize Toolkit Repository

---
+++ Toolkit.scala
@@ -0,0 +1,7 @@
+//> using lib "com.softwaremill.sttp.client3::core:3.8.6"
+//> using lib "com.lihaoyi::upickle:2.0.0"
+//> using lib "com.lihaoyi::os-lib:0.9.0"
+//> using lib "org.scalameta::munit::1.0.0-M7"
+
+object Toolkit:
+    val version = "0.1.0"
