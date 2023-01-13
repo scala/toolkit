dfa967a031ea8d8a9ee09bab44d93bcb0ba9f3e1
Szymon Rodziewicz <szymonrodant@gmail.com>
2023-01-13 12:56:07 +0100

    Fix cross publish

--- Toolkit.js.scala
+++ Toolkit.js.scala
@@ -1,3 +1,5 @@
+//> using scala "2.13", "2.12", "3"
+
 //> using lib "com.softwaremill.sttp.client3::core::3.8.7"
 //> using lib "com.lihaoyi::upickle::2.0.0"
 //> using lib "org.scalameta::munit::1.0.0-M7"
