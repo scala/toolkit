6b9365a378b2b512f4117e61a3910b2f91197130
Jamie Thompson <bishbashboshjt@gmail.com>
2023-03-09 14:36:46 +0100

    add sttp's upickle integration

--- Toolkit.js.scala
+++ Toolkit.js.scala
@@ -1,5 +1,6 @@
 //> using scala "2.13", "2.12", "3"
 
 //> using lib "com.softwaremill.sttp.client3::core::3.8.7"
+//> using lib "com.softwaremill.sttp.client3::upickle::3.8.7"
 //> using lib "com.lihaoyi::upickle::2.0.0"
 //> using lib "org.scalameta::munit::1.0.0-M7"
