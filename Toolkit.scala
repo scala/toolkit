8a94b03edb3096a39dafc354d1f6a690746fc1fc
Szymon Rodziewicz <szymonrodant@gmail.com>
2023-05-04 19:13:07 +0200

    Drop 2.12 support

--- Toolkit.scala
+++ Toolkit.scala
@@ -1,4 +1,4 @@
-//> using scala "2.13", "2.12", "3"
+//> using scala "2.13", "3"
 //> using publish.name "toolkit"
 //> using lib "com.softwaremill.sttp.client4::core::4.0.0-M1"
 //> using lib "com.softwaremill.sttp.client4::upickle::4.0.0-M1"
