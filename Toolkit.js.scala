fec498e5f6f93f34d5f44b4b075b1ccbc934c8cf
Szymon Rodziewicz <szymonrodant@gmail.com>
2023-10-18 14:21:36 +0200

    Bump versions of libraries

--- Toolkit.js.scala
+++ Toolkit.js.scala
@@ -1,5 +1,5 @@
 //> using scala 2.13, 3
 //> using publish.name toolkit
-//> using dep com.softwaremill.sttp.client4::core::4.0.0-M1
-//> using dep com.softwaremill.sttp.client4::upickle::4.0.0-M1
-//> using dep com.lihaoyi::upickle::3.1.0
+//> using dep com.softwaremill.sttp.client4::core::4.0.0-M6
+//> using dep com.softwaremill.sttp.client4::upickle::4.0.0-M6
+//> using dep com.lihaoyi::upickle::3.1.3
--- ToolkitTest.scala
+++ ToolkitTest.scala
@@ -1,3 +1,3 @@
 //> using scala 2.13, 3
 //> using publish.name toolkit-test
-//> using dep org.scalameta::munit::1.0.0-M7
+//> using dep org.scalameta::munit::1.0.0-M10
